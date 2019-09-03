4148de8d866c3090a9c537c372594761e979f2d0
Josrodjr <Josrodjr@gmail.com>
2019-09-03 17:27:02 -0600

    adjust the time

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import math
 # duracion del proyecto 20 dias
 PROJECT_DURATION = 20
 # iteracinoes
-ITERATIONS = 10000
+ITERATIONS = 1000000
 
 tiempos = {
-    'd_interfaz':  {'optimista': 3, 'probable': 4, 'pesimista': 5},
-    'd_nivel':       {'optimista': 3, 'probable': 4, 'pesimista': 5},
+    'd_interfaz':    {'optimista': 1, 'probable': 2, 'pesimista': 3},
+    'd_nivel':       {'optimista': 1, 'probable': 2, 'pesimista': 3},
     'd_mecanicas':   {'optimista': 4, 'probable': 6, 'pesimista': 9},
-    'd_historia':    {'optimista': 2, 'probable': 3, 'pesimista': 4},
+    'd_historia':    {'optimista': 0, 'probable': 1, 'pesimista': 2},
     'p_unitarias':   {'optimista': 2, 'probable': 3, 'pesimista': 6},
     'p_integracion': {'optimista': 2, 'probable': 3, 'pesimista': 6},
     'p_usuario':     {'optimista': 2, 'probable': 3, 'pesimista': 4},
